32ac4959650b5e519f9fd276e55104f5045a7e9f
moldalievazimbek <148510455+moldalievazim@users.noreply.github.com>
2025-02-25 10:49:04 -0600

    Time: 1 ms (99.65%), Space: 44.2 MB (52.87%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+
+        int streakStart = 0;
+        int indexAnswer = 0;
+        
+        while (streakStart < n) {
+            char streakChar = chars[streakStart];
+            int streakEnd = streakStart;
+            int count = 1;
+
+            while (streakEnd + 1 < n && chars[streakEnd + 1] == streakChar) {
+                streakEnd++;
+                count++;
+            }
+
+            chars[indexAnswer++] = streakChar;
+
+            if (count != 1) {
+                char[] countAsChars = Integer.toString(count).toCharArray();
+                for (char digit : countAsChars) {
+                    chars[indexAnswer++] = digit;
+                }
+            }
+
+            streakStart = streakEnd + 1;
+        }
+
+        return indexAnswer;
+    }
+}
