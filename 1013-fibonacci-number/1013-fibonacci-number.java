81d651294e28ef18acbcb2019c96a5a29f8c7cd7
moldalievazimbek <148510455+moldalievazim@users.noreply.github.com>
2025-02-27 16:27:49 -0600

    Time: 8 ms (48.09%), Space: 40.4 MB (62.41%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,16 +1,17 @@
 class Solution {
-    private int[] f;
+    // private int[] f;
 
-    private int fib_(int n) {
-        if (n <= 1) return n;
-        if (f[n] == 0) {
-            f[n] = fib_(n - 1) + fib_(n - 2);
-        }
-        return f[n];
-    }
+    // private int fib_(int n) {
+    //     if (n <= 1) return n;
+    //     if (f[n] == 0) {
+    //         f[n] = fib_(n - 1) + fib_(n - 2);
+    //     }
+    //     return f[n];
+    // }
 
     public int fib(int n) {
-        f = new int[n + 1];
-        return fib_(n);
+        // f = new int[n + 1];
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
     }
 }
