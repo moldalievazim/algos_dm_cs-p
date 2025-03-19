e3f6cc004a95b45a4043bffea1dfd78621de37e1
moldalievazimbek <148510455+moldalievazim@users.noreply.github.com>
2025-03-19 18:09:58 -0500

    Time: 1 ms (63.57%), Space: 40.7 MB (63.17%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> seen = new HashSet<>();
+        HashSet<Integer> seen = new HashSet<>();
         
         while (n != 1 && !seen.contains(n)) {
             seen.add(n);
