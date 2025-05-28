198a4e4e888d11d555a598833e056cbc231d8daa
moldalievazimbek <148510455+moldalievazim@users.noreply.github.com>
2025-05-28 15:51:50 -0500

    Time: 29 ms (44.73%), Space: 57.5 MB (11.2%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,32 +1,29 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int n = nums.length;
+        MergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
 
-        for (int i = 0; i < n; i++) {
-            nums[i] += 50000;
-        }
+    public static void MergeSort(int[] arr, int left, int right) {
+        if (left == right) return;
+        int mid = (left + right) / 2;
+        MergeSort(arr, left, mid);
+        MergeSort(arr, mid + 1, right);
+        Merge(arr, left, right);
+    }
 
-        int k = 100000;
-        int[] b = new int[n];
-        int[] c = new int[k + 1];
-
-        for (int i = 0; i <= k; i++) {
-            c[i] = 0;
+    public static void Merge(int[] arr, int left, int right) {
+        int len = right - left + 1;
+        int[] tmp = new int[len];
+        int mid = (left + right) / 2;
+        for (int k = 0, i = left, j = mid + 1; k < len; k++) {
+            if (i > mid) tmp[k] = arr[j++];
+            else if (j > right) tmp[k] = arr[i++];
+            else if (arr[i] < arr[j]) tmp[k] = arr[i++];
+            else tmp[k] = arr[j++];
         }
-        for (int j = 0; j < n; j++) {
-            c[nums[j]]++;
+        for (int k = 0, i = left; k < len; k++, i++) {
+            arr[i] = tmp[k];
         }
-        for (int j = 1; j <= k; j++) {
-            c[j] += c[j - 1];
-        }
-        for (int j = n - 1; j >= 0; j--) {
-            b[c[nums[j]] - 1] = nums[j];
-            c[nums[j]]--;
-        }
-        for (int i = 0; i < n; i++) {
-            b[i] -= 50000;
-        }
-
-        return b;
     }
 }
